8e2802bf8a3e584bfac48a6240346b69e372d895
Osius Kara <ouus2k@web.de>
2022-11-04 12:22:54 -0500

    fixed clickEvent

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Global variables---------------------->
+// Global variables----------------------------------------------------->
 const choices = ["rock", "paper", "scissors"];
 const winners = [];
 
@@ -13,59 +13,121 @@ const rock2image = document.createElement("img");
 const paper2image = document.createElement("img");
 const scissors2image = document.createElement("img");
 
+const rock3image = document.createElement("img");
+const paper3image = document.createElement("img");
+const scissors3image = document.createElement("img");
+
 const srcPlayer = document.getElementById("player-weapon");
 const srcComp = document.getElementById("comp-weapon");
 
+const scoreBoardHeader = document.getElementById("score-header");
+
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
+
+// Player Selection --------------------------------------------------------->
 rockBtn.addEventListener("click", function () {
   playerSelection = "rock";
+  displayPlayer();
   rock2image.src = "./images/rock2.png";
-  srcPlayer.appendChild(rock2image);
   paper2image.classList.add("hidden");
   scissors2image.classList.add("hidden");
-  displayPlayer();
-  // displayComp();
+  rock2image.classList.remove("hidden");
+  srcPlayer.appendChild(rock2image);
+  displayComp();
+  playRound();
+  updateScore();
 });
 
 paperBtn.addEventListener("click", function () {
   playerSelection = "paper";
+  displayPlayer();
   paper2image.src = "./images/paper2.png";
   rock2image.classList.add("hidden");
-  srcPlayer.appendChild(paper2image);
   scissors2image.classList.add("hidden");
-  displayPlayer();
+  paper2image.classList.remove("hidden");
+  srcPlayer.appendChild(paper2image);
+  displayComp();
+  playRound();
+  updateScore();
 });
 
 scissorsBtn.addEventListener("click", function () {
   playerSelection = "scissors";
+  displayPlayer();
+  rock2image.classList.add("hidden");
+  paper2image.classList.add("hidden");
+  scissors2image.classList.remove("hidden");
   scissors2image.src = "./images/scissors2.png";
   srcPlayer.appendChild(scissors2image);
-  paper2image.classList.add("hidden");
-  rock2image.classList.add("hidden");
-  displayPlayer();
+  displayComp();
+  playRound();
+  updateScore();
 });
 
 function displayPlayer() {
-  computerSelection = computerChoice();
   questionMark1.classList.add("hidden");
   console.log("PlayerChoice: " + playerSelection);
-  console.log("ComputerChoice: " + computerSelection);
 }
 
-function displayComp(computerSelection) {
+// Computer Selection -------------------------------------------------->
+function displayComp() {
+  const computerSelection = computerChoice();
+  questionMark2.classList.add("hidden");
+
   if (computerSelection === "rock") {
-    questionMark2.classList.add("hidden");
-    rock2image.src = "./images/rock2.png";
-    srcComp.appendChild(rock2image);
+    srcPlayer.appendChild(rock3image);
+    rock3image.src = "./images/rock3.png";
+    srcComp.appendChild(rock3image);
   } else if (computerSelection === "paper") {
-    questionMark2.classList.add("hidden");
-    paper2image.src = "./images/paper2.png";
-    srcComp.appendChild(paper2image);
+    srcPlayer.appendChild(paper3image);
+    paper3image.src = "./images/paper3.png";
+    srcComp.appendChild(paper3image);
+  } else {
+    srcPlayer.appendChild(scissors3image);
+    scissors3image.src = "./images/scissors3.png";
+    srcComp.appendChild(scissors3image);
+  }
+  console.log("ComputerChoice: " + computerSelection);
+  console.log("====================");
+}
+
+//Computer Choice random------------------------------------------------->
+function computerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+// Play Round------------------------------------------------------------->
+function playRound() {
+  const computerSelection = computerChoice();
+
+  if (playerSelection === computerSelection) {
+    roundWinner = "tie";
+    // console.log(roundWinner);
+  } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
+    playerScore++;
+    roundWinner = "player";
+    // console.log(roundWinner + " wins");
+  } else if ((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "scissors" && playerSelection === "paper") || (computerSelection === "paper" && playerSelection === "rock")) {
+    computerScore++;
+    roundWinner = "computer";
+    // console.log(roundWinner + " wins");
   }
 }
 
-//Computer Choice random
-function computerChoice() {
-  return choices[Math.floor(Math.random() * choices.length)];
+// Update Header Round Winner-------------------------------------------->
+function updateScore() {
+  if (roundWinner === "tie") {
+    scoreBoardHeader.textContent = "It's a Tie!";
+    scoreBoardHeader.style.color = "#f9c024";
+  } else if (roundWinner === "player") {
+    scoreBoardHeader.textContent = "Player Won!";
+    scoreBoardHeader.style.color = "#03a303";
+  } else if (roundWinner === "computer") {
+    scoreBoardHeader.textContent = "Computer Won!";
+    scoreBoardHeader.style.color = "#e20b0b";
+  }
 }
 
 // function game() {
@@ -82,60 +144,18 @@ function computerChoice() {
 //   const computerSelection = computerChoice();
 //   const winner = checkWinner(playerSelection, computerSelection);
 //   winners.push(winner);
-//   logRound(playerSelection, computerSelection, winner, round);
+//   // logRound(playerSelection, computerSelection, winner, round);
 // }
 
-// Player Choice
-// function playerChoice() {
-//   let input = prompt("Type Rock, Paper or Scissors");
-//   while (input == null) {
-//     input = prompt("Type Rock, Paper or Scissors");
-//   }
-//   input = input.toLowerCase();
-//   let check = validateInput(input);
-//   while (check == false) {
-//     input = prompt("Type Rock, Paper or Scissors. Spelling needs to be exact, but capitilization doesnt matter");
-//     while (input == null) {
-//       input = prompt("Type Rock, Paper or Scissors");
-//     }
-//     input = input.toLowerCase();
-//     check = validateInput(input);
-//   }
-//   return input;
-// }
-
-// // Function to validate if the input is one of the 3 options R P S
-// function validateInput(choice) {
-//   return choices.includes(choice);
-// }
-
-// Who Win ----- Conditions
 // function checkWinner(choiceP, choiceC) {
 //   if (choiceP === choiceC) {
+//     console.log("IT'S A TIE!");
 //     return "Tie";
 //   } else if ((choiceP == "rock" && choiceC == "scissors") || (choiceP == "paper" && choiceC == "rock") || (choiceP == "scissors" && choiceC == "paper")) {
+//     console.log("PLAYER WINS");
 //     return "Player";
 //   } else {
+//     console.log("COMPUTER WINS");
 //     return "Computer";
 //   }
-// }
-
-// Who Wins in the Console
-// function logWins() {
-//   let playerWins = winners.filter((item) => item == "Player").length;
-//   let computerWins = winners.filter((item) => item == "Computer").length;
-//   let ties = winners.filter((item) => item == "Tie").length;
-//   console.log("Results:");
-//   console.log("Player Wins:", playerWins);
-//   console.log("Computer Wins:", computerWins);
-//   console.log("Ties", ties);
-// }
-
-// //Displaying the results of each round in the Console!
-// function logRound(playerChoice, computerChoice, winner, round) {
-//   console.log("Round", round);
-//   console.log("Player Chose:", playerChoice);
-//   console.log("Computer Chose", computerChoice);
-//   console.log(winner, "Won the Round");
-//   console.log("------------------------------");
 // }
